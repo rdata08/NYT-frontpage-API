c5661b71ad4260c539065f581019281a0d49681e
rdata08 <rg673@cornell.edu>
2024-08-01 11:46:11 -0400

    Simplifying Flask Route

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
 from flask import Flask, jsonify
 from backend.config import Config
-import mysql.connector
-import time
+import backend.db
 
 app = Flask(__name__)
 
-while True:
-    try:
-        connection = mysql.connector.connect(host = Config.db_host, database=Config.db_database, user=Config.db_user, password=Config.db_password)
-        print("Connection successful")
-        break
-    except Exception as e:
-        print("Connection unsuccessful")
-        print("Error", e)
-        time.sleep(3)
+DB = backend.db.DatabaseDriver()
 
-@app.route('/','GET')
-def getOne():
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM mytable LIMIT 1;")
-    result = cursor.fetchone()
-    cursor.close()
-    
+@app.route('/', methods=['GET'])
+def getRandom():
+    result = DB.getRandom()
     if result:
         return jsonify({"data": result[0]})
     else:
