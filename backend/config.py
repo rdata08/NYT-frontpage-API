83f2120f004bce0ff090007566100953f0fe4bf9
Ryan Gomez <rg673@cornell.edu>
2024-08-20 15:50:01 -0400

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    db_host = os.getenv("DB_HOST")
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    db_database = os.getenv("DB_DATABASE")
