75a0bc41e755b855bd569ec692712ee89cdb88ef
rdata08 <rg673@cornell.edu>
2024-09-01 20:29:39 -0400

    Documentation and requirements.txt

--- backend/script.py
+++ backend/script.py
@@ -5,18 +5,15 @@ import re
 from config import Config
 import openai
 
+# initialize OpenAI client API
 client = openai.OpenAI(
     api_key=Config.AI_API_KEY,
     base_url="https://api.aimlapi.com"
 )
 
+# request pdf text from link
 url = 'https://static01.nyt.com/images/2024/07/29/nytfrontpage/scan.pdf'
 response = requests.get(url)
-
-countries = set()
-tags = set()
-authors = set()
-headlines = set()
 
 with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
     temp_file.write(response.content)
@@ -31,15 +28,18 @@ for page_num in range(len(pdf_document)):
 
 pdf_document.close()
 
+# clean full_text
 full_text = full_text.replace("-\n", "").replace("\n", " ")
 full_text = re.sub(r'\s+', ' ', full_text).strip()
 
 # new line after "Continued on Page"
 full_text = re.sub(r'(Continued on Page [A-Z]\d+)', r'\1\n\n\n', full_text)
 
-print()
-print()
-paragraphs = full_text.split('\n\n')
+# split text into paragraphs
+paragraphs = full_text.split('\n\n')  
+
+# Author Scraping ------------------------------------------------------------------------
+# retrieve last lines for author names
 last_lines = []
 for paragraph in paragraphs:
     lines = paragraph.split('. ')
@@ -51,35 +51,30 @@ filtered_last_lines = []
 for last_line in last_lines:
     if len(last_line.split()) >= 5:
         filtered_last_lines.append(last_line)
-        # print(last_line)
 
-print()
-print()
+# Possible name for author regex pattern
 pattern = r'^(.*?)(?: By (.+?) Continued on Page [A-Z]\d+| By (.+))$'
-
 matches = [re.match(pattern, line) for line in filtered_last_lines]
 
 final_authors = []
-# Print extracted headlines and authors
+# Extract authors
 for match in matches:
     if match:
-        headline = match.group(1).strip()
+        # headline = match.group(1).strip()
         authors = match.group(2).strip() if match.group(2) else match.group(3).strip()
-
         author_list = authors.split()
         
         for word in author_list:
             # condition:
                 final_authors.append(word)
-
-        print(final_authors)
+        
         verifiedAuthors = ' '.join(final_authors)
 
-        # print(f"Headline: {headline}")
-        # print(f"Authors: {verifiedAuthors}")
+final_authors_str = ' '.join(final_authors)
+print(final_authors_str)
 
 system_content = ""
-user_content = f"What are the full names in this list: {final_authors}"
+user_content = f"What are the full names in this list: {final_authors_str}"
 
 chat_completion = client.chat.completions.create(
     model="mistralai/Mistral-7B-Instruct-v0.2",
@@ -93,8 +88,3 @@ chat_completion = client.chat.completions.create(
 
 response = chat_completion.choices[0].message.content
 print("AI/ML API:\n", response)
-
-# pattern = r'\b[A-Z][A-Za-z\s]+(?:[A-Z][a-z]+)?\sBy\s[A-Z][A-Z]+\s(?:and\s[A-Z][A-Z]+)?'
-# matches = re.findall(pattern, full_text)
-# for match in matches:
-#     print(match)
