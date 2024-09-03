70f838d371d08e977db38009792095a1f5706609
rdata08 <rg673@cornell.edu>
2024-09-03 15:52:41 -0400

    Update Frontend

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import React from "react";
 
 export default function Home() {
     return (
-        <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 min-h-[100vh]">
+            <video src="/videos/videoBg.mp4"
+            autoPlay
+            loop
+            muted
+            className="absolute top-0 left-0 w-full h-[100vh] object-cover z-0"
+            />
+            
             <div className="max-w-7xl w-full">
                 <Hero/>
             </div>
